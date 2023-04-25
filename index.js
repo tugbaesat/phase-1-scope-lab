3a29f3e27fe6069a0f5a6b20a72b28f038344394
Tugba Esat <tugbaesat007@gmail.com>
2023-04-25 14:46:09 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer(item) {
+    bestCustomer = item;
+}
+
+const leastFavoriteCustomer = "Tina";
+
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer = "Jon";
+}
